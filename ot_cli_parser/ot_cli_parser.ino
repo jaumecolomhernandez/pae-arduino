065b98108a9388931c3a9d291c96054c2069d1eb
hNereida <helena.nereida.calatrava@gmail.com>
2019-11-21 11:25:34 +0100

    custom endings, parse lines, read eval print loop

--- ot_cli_parser/ot_cli_parser.ino
+++ ot_cli_parser/ot_cli_parser.ino
@@ -3,19 +3,27 @@
    Ported code from: https://github.com/jaumecolomhernandez/autoconfig-openthread/blob/88c51c36a23c969f9de7fb898e71ded06d2342cc/device_classes.py#L37
 */
 
-// TODO: Implement using real UARTS
-// TODO: Think
-
 bool debug = false;
-//.
+String endings[] = {"> ",
+                    "> \n",
+                    ">",
+                    "Ú",
+                   };
 void setup() {
   Serial.begin(115200); // Serial setup
   Serial2.begin(115200); // Software serial
   Serial2.setRxBufferSize(2048);
   Serial2.println(".");  // This is needed to clean weird input symbols
-  read_ans(); //TO DO: flag mute
+  read_ans();
 }
-
+boolean isEnding(String string) {
+  for (int i = 0; i < sizeof(endings); i++) {
+    if (string.equals(endings[i])) {
+      return true;
+    }
+    return false;
+  }
+}
 String read_line() {
   /*
      Reads single line from Serial buffer
@@ -24,12 +32,7 @@ String read_line() {
   String data = "";
   int tries = 0;
   while (Serial2.available() == 0) {
-    if(debug)
-      Serial.println("[read_line] The chosen UART Serial bus is not available"); // Not sure if needed
-    tries++;
-    if (tries == 3) {
-      break;
-    }
+    if (debug) Serial.println("[read_ans] The chosen UART Serial bus is not available"); // Not sure if needed
   }
 
 
@@ -40,7 +43,6 @@ String read_line() {
       return data;                    // Return the string
   }
 }
-
 void print_hex(String string) {
   /*
     Prints hexadecimal representation of string
@@ -53,12 +55,9 @@ void print_hex(String string) {
     character = printf("%x", string[i] );
     hex.concat(character);
   }
-  Serial.println(hex);   
+  Serial.println(hex);
   Serial.println("");
-
 }
-
-
 void read_ans() {
   /*
      Reads full answer
@@ -69,50 +68,68 @@ void read_ans() {
   int timeout_millis = 1000;
   int current_time = millis();
 
-  // TODO: Parse output
-  // TODO: Add timeout
-  // TODO: Add custom endings
-
   bool wait = true;
 
   while (Serial2.available() == 0) {
-    if(debug) Serial.println("[read_ans] The chosen UART Serial bus is not available"); // Not sure if needed
+    if (debug)
+      Serial.println("[read_ans] The chosen UART Serial bus is not available"); // Not sure if needed
   }
-
   while (wait) {
+    //we read each line received
     String recv_line = read_line();
-    if (recv_line.length() != 0){
-      Serial.print("Received: ");
-      Serial.print(recv_line);
-    }
-    
-    // if(debug) print_hex(recv_line);
 
-    if (recv_line == "> " || recv_line == "> \n" || recv_line == ">" || recv_line == "Ú") {
-      wait = false;
-      if(debug) Serial.println("Now wait false");
-    }
+    if (recv_line.length() != 0) {
+      // *******************[DISTINGUISH STRING TYPES]**********************
+      if (recv_line.endsWith("\n") && recv_line.length() == 2) {
+        //string1 = recv_line
+        //string2 = "\n"
+        //However: ...
+        //string 1 is not recognized as string2 with string1.equals(string2)
+        //nothing to be 
 
-    if (millis() > (current_time + 1000)) wait = false;
-    
+        //not working either: string1.equals("\n\0")
+      } else if (recv_line.endsWith("\n") ) {
+        Serial.print("Received: ");
+        Serial.print(recv_line);
+        if (debug) {
+          Serial.print("caseA");
+          Serial.print(recv_line.length());
+        }
+      } else {
+        Serial.print("Received: ");
+        Serial.println(recv_line);
+        if (debug) {
+          Serial.print("caseB");
+          Serial.print(recv_line.length());
+        }
+        // *******************************************************************
+      }
+      // if(debug) print_hex(recv_line);
+
+      // STOP reading and displaying when reaching end of reception
+      if (isEnding(recv_line)) {
+        wait = false;
+        if (debug) Serial.println("Now wait false");
+      }
+    }
+    if (millis() > (current_time + timeout_millis)) wait = false;
   }
 }
-
 void send_command(String command) {
   Serial.println("\nSending: " + command);
   Serial2.println(command);
   read_ans();
 }
-
 void loop() {
-  // TODO: Implement read–eval–print loop (REPL)
-  send_command("help");
-  delay(100);
-}
 
+  /*
+    // *************************************************************
+    send_command("help");
+    delay(100);
+    // *************************************************************
+  */
 
-/* void loop() {
-   
+  // **************[REPL] READ - EVAL - PRINT - LOOP**************
   String userCommand = "";
   if (Serial.available()) {
     userCommand = Serial.readStringUntil('\n');
@@ -120,5 +137,6 @@ void loop() {
     send_command(userCommand);
   }
   delay(1000);
-  
-}*/
+  // *************************************************************
+
+}
