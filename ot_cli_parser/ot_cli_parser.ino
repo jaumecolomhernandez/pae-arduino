c3cbb5ded4b2f0744b6b867e716feaf535ced3fd
agirones <girones96@gmail.com>
2019-11-24 20:55:20 +0100

    start_joiner() and start_commissioner() functions added.

--- ot_cli_parser/ot_cli_parser.ino
+++ ot_cli_parser/ot_cli_parser.ino
@@ -1,9 +1,11 @@
+
 /*
    IMPLEMENTATION OF SERIAL INTERACTION WITH OPENTHREAD CLI
    Ported code from: https://github.com/jaumecolomhernandez/autoconfig-openthread/blob/88c51c36a23c969f9de7fb898e71ded06d2342cc/device_classes.py#L37
 */
 
 bool debug = false;
+//TODO ending when: "commissioner start" is:  > Commissioner: active
 String endings[] = {"> ",
                     "> \n",
                     ">",
@@ -13,9 +15,13 @@ void setup() {
   Serial.begin(115200); // Serial setup
   Serial2.begin(115200); // Software serial
   Serial2.setRxBufferSize(2048);
+  Serial.println("Before send . by serial2");
   Serial2.println(".");  // This is needed to clean weird input symbols
   read_ans();
+  Serial.println("Before start_commissioner()");
+  // start_commissioner();
 }
+
 boolean isEnding(String string) {
   for (int i = 0; i < sizeof(endings); i++) {
     if (string.equals(endings[i])) {
@@ -24,6 +30,7 @@ boolean isEnding(String string) {
     return false;
   }
 }
+  
 String read_line() {
   /*
      Reads single line from Serial buffer
@@ -34,8 +41,6 @@ String read_line() {
   while (Serial2.available() == 0) {
     if (debug) Serial.println("[read_ans] The chosen UART Serial bus is not available"); // Not sure if needed
   }
-
-
   while (Serial2.available() > 0) {
     char character = Serial2.read(); // Receive a single character from the software serial port
     data.concat(character);           // Add the received character to the receive buffer
@@ -43,13 +48,13 @@ String read_line() {
       return data;                    // Return the string
   }
 }
+
 void print_hex(String string) {
   /*
     Prints hexadecimal representation of string
   */
   String hex;
   char character = '-';
-
   for (int i = 0; i < string.length(); i++) {
     //Serial.print(string[i], HEX); // Iterates string and prints as HEX
     character = printf("%x", string[i] );
@@ -58,18 +63,15 @@ void print_hex(String string) {
   Serial.println(hex);
   Serial.println("");
 }
-void read_ans() {
-  /*
-     Reads full answer
-     Reads lines until the value received matches with
-     one of the defined endings.
-  */
 
+String read_ans() {
+  // Reads full answer
+  // Reads lines until the value received matches with
+  // one of the defined endings.
   int timeout_millis = 1000;
   int current_time = millis();
-
   bool wait = true;
-
+  String full_answer="";
   while (Serial2.available() == 0) {
     if (debug)
       Serial.println("[read_ans] The chosen UART Serial bus is not available"); // Not sure if needed
@@ -77,7 +79,7 @@ void read_ans() {
   while (wait) {
     //we read each line received
     String recv_line = read_line();
-
+    full_answer.concat(recv_line);
     if (recv_line.length() != 0) {
       // *******************[DISTINGUISH STRING TYPES]**********************
       if (recv_line.endsWith("\n") && recv_line.length() == 2) {
@@ -86,7 +88,6 @@ void read_ans() {
         //However: ...
         //string 1 is not recognized as string2 with string1.equals(string2)
         //nothing to be 
-
         //not working either: string1.equals("\n\0")
       } else if (recv_line.endsWith("\n") ) {
         Serial.print("Received: ");
@@ -114,21 +115,22 @@ void read_ans() {
     }
     if (millis() > (current_time + timeout_millis)) wait = false;
   }
+  return full_answer;
 }
-void send_command(String command) {
+
+String send_command(String command) {
   Serial.println("\nSending: " + command);
   Serial2.println(command);
-  read_ans();
+  return read_ans();
 }
-void loop() {
 
+void loop() {
   /*
     // *************************************************************
     send_command("help");
     delay(100);
     // *************************************************************
   */
-
   // **************[REPL] READ - EVAL - PRINT - LOOP**************
   String userCommand = "";
   if (Serial.available()) {
@@ -138,5 +140,47 @@ void loop() {
   }
   delay(1000);
   // *************************************************************
+}
 
+void start_commissioner() {
+  send_command("dataset init new");
+  delay(1000);
+  send_command("dataset");
+  delay(1000);
+  send_command("dataset commit active");
+  delay(1000);
+ // send_command("panid 0xdead");
+  send_command("ifconfig up");
+  delay(1000);
+  send_command("thread start");
+  delay(1000);
+  send_command("ipaddr");
+  delay(5000);
+  send_command("commissioner start");
+  delay(10000);
+  send_command("commissioner joiner add * AAAA");
+}
+
+void start_joiner() {
+  send_command("ifconfig up");
+  send_command("eui64");
+  send_command("joiner start AAAA");
+  bool join_answer = false;
+  String s = "";
+  //Wait for an answer
+  while(Serial2.available()==0 && !join_answer){
+    //Check if is a Join success answer
+    s = read_ans();
+    Serial.print("Answer to the joining: ");
+    Serial.println(s);
+    if(s=="Join success"){
+      join_answer=true;
+      send_command("thread start");
+      delay(5000);
+      send_command("state");
+    }else{
+    join_answer=true;
+    Serial.println("Join failure");
+    }
+  }Serial.println("Out of the start_joiner() while");
 }
