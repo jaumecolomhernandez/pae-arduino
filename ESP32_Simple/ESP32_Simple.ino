dce9c18601734482a0ca90bc6f9a7e0111f23782
jaumecolomhernandez <jaumecolomhernandez@gmail.com>
2019-11-29 11:26:04 +0100

    Fixed communication procotol

--- ESP32_Simple/ESP32_Simple.ino
+++ ESP32_Simple/ESP32_Simple.ino
@@ -19,85 +19,117 @@ uint8_t readline(char *buff, uint8_t maxbuff, uint16_t timeout = 0);
 uint8_t type;
 char replybuffer[255]; // this is a large buffer for replies
 char imei[16] = {0}; // MUST use a 16 character buffer for IMEI!
+//unsigned long time;
 
 enum State
 {
-    header,
-    payload,
-    none
+  header,
+  payload,
+  none
 };
 
 struct message
 {
-    char header[10];
-    char message[80];
+  char header[10];
+  char message[80];
 } new_msg = {{0}, {0}};
 
 enum State s = none;
 
 void print_messages(struct message *msgs, int n_msgs)
 {
-    // Prints the messages array
-    for (int i = 0; i < n_msgs; i++)
-    {
-        printf("Message %i/%i - ", i, n_msgs);
-        printf("Header: %s - ", msgs[i].header);
-        printf("Payload: %s\n", msgs[i].message);
-    }
+  // Prints the messages array
+  for (int i = 0; i < n_msgs; i++)
+  {
+      printf("Message %i/%i - ", i, n_msgs);
+      printf("Header: %s - ", msgs[i].header);
+      printf("Payload: %s\n", msgs[i].message);
+  }
 }
 
-int parse_buffer(char *buffer, int nchars, struct message *msgs)
+uint8_t parse_buffer(char *buffer, int nchars, struct message *msgs)
 {
-    /* Parses the receiver buffer 
-       The protocol is the following |HEADER|PAYLOAD| the messages 
-       are separated using vertical bars (|)
-       Params:
-       - buffer: (*char) pointer to char array containing the received content
-       - nchars: (int) lenght of the buffer
-       - msgs: (struct *message) pointer to array of messages(struct)
-       Returns:
-       - n_msgs: (int) number of messages received
-    */
-    int n_msgs = 0;
-    int begin = 0;
-    int end = 0;
+  /* Parses the receiver buffer 
+      The protocol is the following |HEADER|PAYLOAD| the messages 
+      are separated using vertical bars (|)
+      Params:
+      - buffer: (*char) pointer to char array containing the received content
+      - nchars: (int) lenght of the buffer
+      - msgs: (struct *message) pointer to array of messages(struct)
+      Returns:
+      - n_msgs: (int) number of messages received
+  */
+  uint8_t n_msgs = 0;
+  int begin = 0;
+  int end = 0;
 
-    s = none;
+  s = none;
 
-    for (int i = 0; i < nchars; i++)
-    {
-        if ((s == none) && (buffer[i] == '|'))
-        {
-            // Change state to header (none->header)
-            s = header;             // Set state
-            begin = i + 1;          // Set begin index of message
-            msgs[n_msgs] = new_msg; // Initialize with null arrays
-        }
-        else if ((s == header) && (buffer[i] == '|'))
-        {
-            // Change state to payload (header->payload)
-            s = payload; // Set state
-            end = i;     // Set end index of header
+  for (int i = 0; i < nchars; i++)
+  {
+      if ((s == none) && (buffer[i] == '|'))
+      {
+          // Change state to header (none->header)
+          s = header;             // Set state
+          begin = i + 1;          // Set begin index of message
+          msgs[n_msgs] = new_msg; // Initialize with null arrays
+      }
+      else if ((s == header) && (buffer[i] == '|'))
+      {
+          // Change state to payload (header->payload)
+          s = payload; // Set state
+          end = i;     // Set end index of header
 
-            // Copy from index begin to index end (both inclusive)
-            memcpy(msgs[n_msgs].header, buffer + begin, end - begin);
+          // Copy from index begin to index end (both inclusive)
+          memcpy(msgs[n_msgs].header, buffer + begin, end - begin);
 
-            begin = i + 1; // Set begin index of payload
-        }
-        else if ((s == payload) && (buffer[i] == '|'))
-        {
-            // Change state back to none (payload -> none)
-            end = i;  // Set payload end index
-            s = none; // Set state
+          begin = i + 1; // Set begin index of payload
+      }
+      else if ((s == payload) && (buffer[i] == '|'))
+      {
+          // Change state back to none (payload -> none)
+          end = i;  // Set payload end index
+          s = none; // Set state
 
-            // Copy from index begin to index end (both inclusive)
-            memcpy(msgs[n_msgs].message, buffer + begin, end - begin);
+          // Copy from index begin to index end (both inclusive)
+          memcpy(msgs[n_msgs].message, buffer + begin, end - begin);
 
-            // Add one to number of messages
-            n_msgs = n_msgs + 1;
-        }
+          // Add one to number of messages
+          n_msgs = n_msgs + 1;
+      }
+  }
+  return n_msgs;
+}
+
+uint8_t read_messages(struct message *msgs){
+  /* Reads from receive buffer NON-BLOCKING */
+  char replybuffer[255];
+  uint16_t bytesRead = fona.UDPread(replybuffer, 250);
+
+  if (bytesRead == 0) {
+    //Serial.println(F("Failed to read / nothing available!"));
+  } else{
+    Serial.println("Read content:");  
+    for (int i=0; i<bytesRead; i++){
+      Serial.print(replybuffer[i]);
     }
-    return n_msgs;
+  }
+  
+
+  uint8_t n_msgs = parse_buffer(replybuffer, bytesRead, msgs);
+  
+  Serial.println(F(""));
+
+  print_messages(msgs, n_msgs);
+
+  Serial.println(F(""));
+
+  // TODO: Implement protocol parser
+  // Protocol is -> '||HEAD|CONTENT||'
+  // TODO: Create functional script
+  // DONE: Created testing script
+
+  return n_msgs;
 }
 
 void setup() {
@@ -124,7 +156,7 @@ void setup() {
   // Start at default SIM7000 shield baud rate
   fonaSS.begin(115200, SERIAL_8N1, FONA_TX, FONA_RX); // baud rate, protocol, ESP32 RX pin, ESP32 TX pin
 
-  Serial.println(F("Configuring to 9600 baud"));
+  Serial.println(F("Configuring to 115200 baud"));
   fonaSS.println("AT+IPR=9600"); // Set baud rate
   delay(100); // Short pause to let the command run
   fonaSS.begin(9600, SERIAL_8N1, FONA_TX, FONA_RX); // Switch to 9600
@@ -137,18 +169,6 @@ void setup() {
   Serial.println(F("FONA is OK"));
   Serial.print(F("Found "));
   switch (type) {
-    case SIM800L:
-      Serial.println(F("SIM800L")); break;
-    case SIM800H:
-      Serial.println(F("SIM800H")); break;
-    case SIM808_V1:
-      Serial.println(F("SIM808 (v1)")); break;
-    case SIM808_V2:
-      Serial.println(F("SIM808 (v2)")); break;
-    case SIM5320A:
-      Serial.println(F("SIM5320A (American)")); break;
-    case SIM5320E:
-      Serial.println(F("SIM5320E (European)")); break;
     case SIM7000A:
       Serial.println(F("SIM7000A (American)")); break;
     case SIM7000C:
@@ -157,10 +177,6 @@ void setup() {
       Serial.println(F("SIM7000E (European)")); break;
     case SIM7000G:
       Serial.println(F("SIM7000G (Global)")); break;
-    case SIM7500A:
-      Serial.println(F("SIM7500A (American)")); break;
-    case SIM7500E:
-      Serial.println(F("SIM7500E (European)")); break;
     default:
       Serial.println(F("???")); break;
   }
@@ -175,39 +191,10 @@ void setup() {
   fona.setFunctionality(1); // AT+CFUN=1
 
   // Configure a GPRS APN, username, and password.
-  // You might need to do this to access your network's GPRS/data
-  // network.  Contact your provider for the exact APN, username,
-  // and password values.  Username and password are optional and
-  // can be removed, but APN is required.
   fona.setNetworkSettings(F("orangeworld"), F("orange"), F("orange"));
-  //fona.setNetworkSettings(F("m2m.com.attz")); // For AT&T IoT SIM card
-  //fona.setNetworkSettings(F("telstra.internet")); // For Telstra (Australia) SIM card - CAT-M1 (Band 28)
-  //fona.setNetworkSettings(F("hologram")); // For Hologram SIM card
 
-  // Optionally configure HTTP gets to follow redirects over SSL.
-  // Default is not to follow SSL redirects, however if you uncomment
-  // the following line then redirects over SSL will be followed.
-  //fona.setHTTPSRedirect(true);
-
-  /*
-  // Other examples of some things you can set:
-  fona.setPreferredMode(38); // Use LTE only, not 2G
-  */
   fona.setPreferredLTEMode(1); // Use LTE CAT-M only, not NB-IoT
-  //fona.setOperatingBand("CAT-M", 12); // AT&T uses band 12
-  //fona.setOperatingBand("CAT-M", 13); // Verizon uses band 13
-  //fona.enableRTC(true);
-  
-  //fona.enableSleepMode(true);
-  //fona.set_eDRX(1, 4, "0010");
-  //fona.enablePSM(true);
 
-  // Set the network status LED blinking pattern while connected to a network (see AT+SLEDS command)
-  //fona.setNetLED(true, 2, 64, 3000); // on/off, mode, timer_on, timer_off
-  //fona.setNetLED(false); // Disable network status LED
-  
-
-  //printMenu();
 }
 
 
@@ -222,6 +209,8 @@ void loop() {
 
     
   Serial.println("Starting autopilot!");
+
+  // Wait until network status 2
   uint8_t n;
   do{
     n = fona.getNetworkStatus();
@@ -230,65 +219,60 @@ void loop() {
     delay(1000);
   }while(n != 1);
 
+  // Activate GPRS
   if (!fona.enableGPRS(true)){
     Serial.println(F("Failed to turn on"));
   }
 
+  // Connect to PAESERVER
   if (! fona.UDPconnect("147.83.39.50", 12342)) Serial.println(F("Failed to connect!")); 
   
-  delay(500);
+  delay(200);
 
   char auth[] = "AUTH 45";
   if(! fona.UDPsend(auth, 7)) Serial.println(F("Failed to connect!"));
 
-  while(true){
-    // Read from receive buffer
-    char replybuffer[255];
-    uint16_t bytesRead = fona.UDPread(replybuffer, 250);
+  unsigned long time = millis();
+  char hey[] = "HEY";
 
-    if (bytesRead == 0) {
-      Serial.println(F("Failed to read / nothing available!"));
-    }
-    Serial.println("Read content:");  
-    for (int i=0; i<bytesRead; i++){
-      Serial.print(replybuffer[i]);
+  while(true){
+
+    // Send message every 4 seconds
+    if ((millis() - time) > 4000){
+      time = millis();
+      if (! fona.UDPsend(hey, sizeof(hey)-1)) Serial.println(F("Failed to send!"));
+      Serial.println(F("Sent message 'HEY' "));
     }
 
     struct message msgs[10];
-    int n_msgs = parse_buffer(replybuffer, bytesRead, msgs);
-    
-    Serial.println(F(""));
+    // Read messages
+    uint8_t n_msgs = read_messages(msgs); 
 
-    print_messages(msgs, n_msgs);
-
-    Serial.println(F(""));
-
-    // TODO: Implement protocol parser
-    // Protocol is -> '||HEAD|CONTENT||'
-    // TODO: Create functional script
-    // DONE: Created testing script
-
+    // Resonse callbacks
     for (int i=0; i<n_msgs; i++){
       if (msgs[i].message == "HELLO"){
         char res[] = "WORLD";
         if (! fona.UDPsend(res, sizeof(res))) Serial.println(F("Failed to send!"));
-        Serial.print("Sent response");
+        Serial.println("Sent response");
       } else {
-        Serial.print("Method not implemented");
+        Serial.println("Method not implemented");
       }
     }
     delay(1000);
-  }
-}
-  /*
-  // flush input
-  flushSerial();
-  while (fona.available()) {
-    Serial.write(fona.read());
+
+    // flush input
+    flushSerial();
+    while (fona.available()) {
+      Serial.write(fona.read());
+    }
+
   }
 
+  
+  
+
 }
-*/
+
 void flushSerial() {
   while (Serial.available())
     Serial.read();
@@ -303,7 +287,7 @@ uint16_t readnumber() {
   char c;
   while (! isdigit(c = readBlocking())) {
     //Serial.print(c);
-  }
+  }sizeof
   Serial.print(c);
   x = c - '0';
   while (isdigit(c = readBlocking())) {
